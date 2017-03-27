5bf0d12148ac0b5c01a4c8c7ee133ba05e065786
Eric Gerling <ericgerling@gmail.com>
2017-03-27 09:43:37 -0500

    Placeholder commit adding additional information for idp user pool creation

--- aws/resource_trility_aws_cognito_idp_user_pool.go
+++ aws/resource_trility_aws_cognito_idp_user_pool.go
@@ -17,9 +17,203 @@ func resourceTrilityAwsCognitoIDPUserPool() *schema.Resource {
         Delete: resourceCognitoIDPUserPoolDelete,
 
         Schema: map[string]*schema.Schema {
-            "name": &schema.Schema {
+            "poolname": &schema.Schema {
                 Type: schema.TypeString,
                 Required: true,
+            },
+            "policies": &schema.Schema{
+                Type: schema.TypeSet,
+                Optional: true,
+                Elem: &schema.Resource{
+                    Schema: map[string]*schema.Schema{
+                        "passwordpolicy": &schema.Schema{
+                            Type: schema.TypeSet,
+                            Optional: true,
+                            Elem: &schema.Resource{
+                                Schema: map[string]*schema.Schema{
+                                    "minimumlength": &schema.Schema{
+                                        Type: schema.TypeInt,
+                                        Required: true,
+                                    },
+                                    "requireuppercase": &schema.Schema{
+                                        Type: schema.TypeBool,
+                                        Required: true,
+                                    },
+                                    "requirelowercase": &schema.Schema{
+                                        Type: schema.TypeBool,
+                                        Required: true,
+                                    },
+                                    "requirenumbers": &schema.Schema{
+                                        Type: schema.TypeBool,
+                                        Required: true,
+                                    },
+                                    "requiresymbols": &schema.Schema{
+                                        Type: schema.TypeBool,
+                                        Required: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            "lambdaconfig": &schema.Schema{
+                Type: schema.TypeSet,
+                Optional: true,
+                Elem: &schema.Resource{
+                    Schema: map[string]*schema.Schema{
+                        "presignup": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                        "custommessage": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                        "postconfirmation": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                        "preauthentication": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                        "postauthentication": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                        "defineauthchallenge": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                        "createauthchallenge": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                        "verifyauthchallengeresponse": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                    },
+                },
+            },
+            "autoverifiedattributes": &schema.Schema{
+                Type: schema.TypeList,
+                Optional: true,
+                Elem: &schema.Schema{Type: schema.TypeString},
+            },
+            "aliasattributes": &schema.Schema{
+                Type: schema.TypeList,
+                Optional: true,
+                Elem: &schema.Schema{Type: schema.TypeString},
+            },
+            "smsverificationmessage": &schema.Schema{
+                Type: schema.TypeString,
+                Optional: true,
+            },
+            "emailverifcationmessage": &schema.Schema{
+                Type: schema.TypeString,
+                Optional: true,
+            },
+            "emailverificationsubject": &schema.Schema{
+                Type: schema.TypeString,
+                Optional: true,
+            },
+            "smsauthenticationmessage": &schema.Schema{
+                Type: schema.TypeString,
+                Optional: true,
+            },
+            "mfaconfiguration": &schema.Schema{
+                Type: schema.TypeString,
+                Optional: true,
+            },
+            "deviceconfiguration": &schema.Schema{
+                Type: schema.TypeSet,
+                Optional: true,
+                Elem: &schema.Resource{
+                    Schema: map[string]*schema.Schema{
+                        "challengerequiredonnewdevice": &schema.Schema{
+                            Type: schema.TypeBool,
+                            Optional: true,
+                        },
+                        "deviceonlyrememberedonuserprompt": &schema.Schema{
+                            Type: schema.TypeBool,
+                            Optional: true,
+                        },
+                    },
+                },
+            },
+            "emailconfiguration": &schema.Schema{
+                Type: schema.TypeSet,
+                Optional: true,
+                Elem: &schema.Resource{
+                    Schema: map[string]*schema.Schema{
+                        "sourcearn": &schema.Schema{
+                            Type: schema.TypeString,
+                            Required: true,
+                        },
+                        "replytoemailaddress": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                    },
+                },
+            },
+            "smsconfiguration": &schema.Schema{
+                Type: schema.TypeSet,
+                Optional: true,
+                Elem: &schema.Resource{
+                    Schema: map[string]*schema.Schema{
+                        "snscallerarn": &schema.Schema{
+                            Type: schema.TypeString,
+                            Required: true,
+                        },
+                        "externalid": &schema.Schema{
+                            Type: schema.TypeString,
+                            Optional: true,
+                        },
+                    },
+                },
+            },
+            "userpooltags": &schema.Schema{
+                Type: schema.TypeMap,
+                Optional: true,
+            },
+            "admincreateuserconfig": &schema.Schema{
+                Type: schema.TypeSet,
+                Optional: true,
+                Elem: &schema.Resource{
+                    Schema: map[string]*schema.Schema{
+                        "allowadmincreateuseronly": &schema.Schema{
+                            Type: schema.TypeBool,
+                            Required: true,
+                        },
+                        "unusedaccountvaliditydays": &schema.Schema{
+                            Type: schema.TypeInt,
+                            Required: true,
+                        },
+                        "invitemessagetemplate": &schema.Schema{
+                            Type: schema.TypeSet,
+                            Optional: true,
+                            Elem: &schema.Resource{
+                                Schema: map[string]*schema.Schema{
+                                    "smsmessage": &schema.Schema{
+                                        Type: schema.TypeString,
+                                        Required: true,
+                                    },
+                                    "emailmessage": &schema.Schema{
+                                        Type: schema.TypeString,
+                                        Required: true,
+                                    },
+                                    "emailsubject": &schema.Schema{
+                                        Type: schema.TypeString,
+                                        Required: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
             },
         },
     }
@@ -27,16 +221,16 @@ func resourceTrilityAwsCognitoIDPUserPool() *schema.Resource {
 
 func resourceCognitoIDPUserPoolCreate(d *schema.ResourceData, meta interface{}) error {
     cidpconn := meta.(*AWSClient).cidpconn
-    name := d.Get("name").(string)
+    poolname := d.Get("poolname").(string)
 
     params := &cognitoidentityprovider.CreateUserPoolInput{
-        PoolName: aws.String(name),
+        PoolName: aws.String(poolname),
     }
 
     // createResp, err := cidpconn.CreateUserPool(params)
     _, err := cidpconn.CreateUserPool(params)
     if err != nil {
-        return fmt.Errorf("Error creating User Pool %s: %s", name, err)
+        return fmt.Errorf("Error creating User Pool %s: %s", poolname, err)
     }
     // return resourceCognitoIdentityProviderRead(d, createResp.Group)
     //d.SetId(createResp.UserPoolId)
